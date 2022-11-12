03bc6c6d855345f3a3605b988061fe7790aedb3e
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-12 12:36:04 +0300

    Update on the bash file.

---
+++ filter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#############################################################################
+## 									   ##
+##	Title : Filter Script						   ## 	
+##									   ##
+##	Author : Ernest Odhiambo Elnino					   ##
+##									   ##
+##	Github : https://github.com/ProtocolWhisperer01 		   ##
+#############################################################################
+
+
+for ip in 'seq 1 254' ; do ping -c 1 $1.$ip | grep "64 byte" | cut -d " " -f 4 | tr -d ":" & done
