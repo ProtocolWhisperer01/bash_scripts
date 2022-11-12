ac44ccc909bc4a146505aaacc7d4bff13ff66df0
ProtocolWhisper01 <ernest.odhiambo1@gmail.com>
2022-11-12 13:09:11 +0300

    Tested out the conditional and error exceptios.

--- filter.sh
+++ filter.sh
@@ -10,6 +10,13 @@
 #############################################################################
 
 
+if ["$1" == ""]
+then
+echo "You forgot an IP address!"
+echo "Syntax: ./filter.sh 192.168.1"
+
+else
 for ip in `seq 1 254` ; do 
 ping -c 1 $1.$ip | grep "64 byte" | cut -d " " -f 4 | tr -d ":" & 
 done
+fi
